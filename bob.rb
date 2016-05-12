8bdfdbfb7edcd32985a959b3cc4436b0d8ac41a4
Zack Forbing <zackforbing@gmail.com>
2016-05-12 12:05:37 -0600

    daily commit

--- bob.rb
+++ bob.rb
@@ -1,7 +1,15 @@
 class Bob
-  def initialize
-  end
 
-  def hey(text)
+  def hey(remark)
+
+    if remark.strip == ""
+      'Fine. Be that way!'
+    elsif remark.upcase == remark && remark =~ /[A-Z]/
+      'Whoa, chill out!'
+    elsif remark.end_with?("?")
+      'Sure.'
+    else
+      'Whatever.'
+    end
   end
 end
