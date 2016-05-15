f33df19158981a5d4946ea363661d94d4cddd4b3
Zack Forbing <zackforbing@gmail.com>
2016-05-14 18:43:46 -0600

    saturday commit

--- age_ordering.rb
+++ age_ordering.rb
@@ -6,7 +6,30 @@ data = [
   ['Steve', 24],
   ['Jill', 24]
 ]
-sorted = data.sort { |a, b| a[1] <=> b[1]}
-sorted.each do |datum|
-  puts datum[0]
-end
+# * Level 1
+# sorted = data.sort { |a, b| a[1] <=> b[1]}
+# sorted.each do |datum|
+#   puts datum[0]
+# end
+
+# * Level 2
+# sorted = data.sort { |a, b| a[1] <=> b[1]}
+# sorted.each do |datum|
+#   puts datum[0] + " (#{datum[1]})"
+# end
+
+# * Level 3
+# data = data.sort { |a, b| a[0] <=> b[0]}
+# sorted = data.sort { |a, b| a[1] <=> b[1]}
+# sorted.each do |datum|
+#   puts datum[0] + " (#{datum[1]})"
+# end
+
+# * Level 4
+#combine
+data.each do |ary|
+  ary.reverse!
+  end
+names  = Hash.new{ |h,k| h[k]=[] }
+data.each { |key,value| names[key] << value}
+puts names.inspect
